c3001b7bb4ccbedbb241cb992acb67c4e2366352
Frank <pussinboots666@googlemail.com>
2014-07-10 14:19:19 +0200

    update the provisioner script.

--- provision/provision.sh
+++ provision/provision.sh
@@ -10,27 +10,42 @@ else
 	apt-get install python-software-properties
 	add-apt-repository ppa:webupd8team/java
 	apt-get update -qq
-	#apt-get install oracle-java7-installer # enable this to install a specific version of java java 8 is the default
 	echo "the installation with apt-get install oracle-java8-installer has to perfomed manual"
 fi
+
+#apt-get install oracle-java7-installer
 
 apt-get install htop
 # install git
 apt-get install git-core
 apt-get -f install 
-# generate ssh keys
-ssh-keygen -t rsa -C "pussinboots666@googlemail.com"
+# generate ssh keys replace it with your email address
+if [ -f /home/vagrant/.ssh/id_rsa.pub ]; then
+	echo "skip generate ssh key"
+else
+	echo "generate ssh key that have to be uploaded to github"
+	ssh-keygen -t rsa -C "pussinboots666@googlemail.com"
+	chmod 600 .ssh/*
+	# solve the ssh github problem see https://help.github.com/articles/using-ssh-over-the-https-port#enabling-ssh-connections-over-https
+	echo "Host github.com
+	  Hostname ssh.github.com
+	  Port 443" > .ssh/config
+fi
 # download idea 13
-if [ ! -d "/home/vagrant/workspace/devel/idea" ]; then
+if [ -d "/home/vagrant/workspace/devel/idea" ]; then
+    echo "skip idea 13 installation"
+else
+	echo "idea 13 installation"
 	mkdir -p /home/vagrant/workspace/devel/idea
 	cd /home/vagrant/workspace/devel/idea
 	cp /vagrant/ideaIU-13.1.3.tar.gz ./
-	#wget http://download.jetbrains.com/idea/ideaIU-12.1.7b.tar.gz >>wgeterr.log
+	#wget http://download.jetbrains.com/idea/ideaIU-13.1.3.tar.gz >>wgeterr.log
 	# unzip idea
 	tar xvfz ideaIU-13.1.3.tar.gz
 fi
 
 if [ ! -d "/home/vagrant/workspace/devel/play" ]; then
+	echo "install play 2.2.3"
 	mkdir -p /home/vagrant/workspace/devel/play
 	cd /home/vagrant/workspace/devel/play
 	wget http://downloads.typesafe.com/play/2.2.3/play-2.2.3.zip
@@ -38,20 +53,30 @@ if [ ! -d "/home/vagrant/workspace/devel/play" ]; then
 	echo "export PATH=$PATH:/home/vagrant/workspace/devel/play/play-2.2.3" > /etc/profile.d/play.sh
 	chmod a+x /etc/profile.d/play.sh
 fi
-apt-get install curl
-curl https://raw.githubusercontent.com/n8han/conscript/master/setup.sh | sh
-echo "export PATH=$PATH:/home/vagrant/bin/" > /etc/profile.d/conscript.sh
-chmod a+x /etc/profile.d/conscript.sh
+
+#install conscript
+if which cs >/dev/null; then
+    echo "skip conscript installation"
+else
+	echo "install conscript"
+	apt-get install curl
+	su -l vagrant -c "curl https://raw.githubusercontent.com/n8han/conscript/master/setup.sh | sh"
+	echo "export PATH=$PATH:/home/vagrant/bin/" > /etc/profile.d/conscript.sh
+	chmod a+x /etc/profile.d/conscript.sh
+fi
 
 # download sbt 0.13
 cd /tmp
-if [ ! -f sbt-0.13.5.deb ]; then
-wget http://dl.bintray.com/sbt/debian/sbt-0.13.5.deb >>wgeterr.log
-# install sbt
-apt-get -f install
-dpkg -i sbt*.deb
-# if no jdk is installed than jdk 6 will be installed sbt depends on
-sbt test
+if which sbt >/dev/null; then
+	echo "skip sbt installation"
+else
+	echo "sbt installation"
+	wget http://dl.bintray.com/sbt/debian/sbt-0.13.5.deb >>wgeterr.log
+	# install sbt
+	apt-get -f install
+	dpkg -i sbt*.deb
+	# if no jdk is installed than jdk 6 will be installed sbt depends on
+	su -l vagrant -c "sbt test"
 fi
 apt-get -f install 
 
@@ -62,22 +87,19 @@ else
 	wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 fi
 
-#cd /home/vagrant/workspace/
-#git clone git@github.com:pussinboots/playzanox.git
-# connect playzanox local workspace with heroku
 
-#heroku git:remote -a playzanox
 cd /home/vagrant/
 chown -R vagrant:vagrant workspace
-su -l vagrant
-# setup karma test runner http://karma-runner.github.io/0.12/intro/installation.html
-#wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.7.0/install.sh | sh
+
+#install nvm nodejs
 if which nvm >/dev/null; then
     echo "skip nodejs installation"
 else
-	wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.10.0/install.sh | sh
-	source .nvm/nvm.sh
-	nvm install 0.10.28
+	echo "nodejs installation"
+    su -l vagrant -c "wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.10.0/install.sh | sh"
+	su -l vagrant -c "source .nvm/nvm.sh"
+	su -l vagrant -c "nvm install 0.10.29"
+	su -l vagrant -c "nvm alias default 0.10.29"
 fi
 
 
@@ -86,40 +108,55 @@ sudo apt-get -f install
 if which travis >/dev/null; then
     echo "skip travis installation"
 else
+	echo "install ruby 1.9.1-dev package"
 	sudo apt-get install ruby1.9.1-dev
 	sudo gem install travis
 fi
 
-#npm install karma --save-dev
-#npm install karma-jasmine karma-chrome-launcher --save-dev
-# karma start test run
-#./node_modules/karma/bin/karma start
+#install gnome window manager 
+yes "" | sudo apt-get -f install gnome-session-flashback
 
-sudo apt-get -f install gnome-shell
 #install softcover
 if which softcover >/dev/null; then
     echo "skip softcover installation"
 else
+	echo "softcover installation"
+    yes "" | sudo apt-get install libcurl4-openssl-dev
 	sudo gem install softcover
+	su -l vagrant -c "softcover check"
 fi
+
+#install softcover runtime dependencies
+echo "softcover dependency installation performed with apt-get"
 yes "" | sudo apt-add-repository ppa:texlive-backports/ppa
 sudo apt-get update -qq
 sudo apt-get -f install
-sudo apt-get install texlive-xetex texlive-fonts-recommended texlive-latex-recommended texlive-latex-extra
-sudo apt-get install inkscape
+yes "" | sudo apt-get install texlive-xetex texlive-fonts-recommended texlive-latex-recommended texlive-latex-extra
+yes "" | sudo apt-get install inkscape
+sudo apt-get install phantomjs
+
 if which calibre >/dev/null; then
     echo "skip calibre installation"
 else
-	sudo python -c "import sys; py3 = sys.version_info[0] > 2; u = __import__('urllib.request' if py3 else 'urllib', fromlist=1); exec(u.urlopen('http://status.calibre-ebook.com/linux_installer').read()); main(install_dir='`echo ~`')"
+	echo "calibre installation"
+	su -l vagrant -c "wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c \"import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()\""
 fi
-export PATH=$PATH:`echo ~`
-if [ ! -d "~/epubcheck-3.0" ]; then
-cd /home/vagrant/
-chown -R vagrant:vagrant ./*
-curl -O -L https://github.com/IDPF/epubcheck/releases/download/v3.0/epubcheck-3.0.zip && unzip epubcheck-3.0.zip -d ~
+if [ -d "/home/vagrant/epubcheck-3.0" ]; then
+	echo "skip epubcheck installation"
+else
+	echo "install epubcheck 3.0 for softcover"
+	cd /home/vagrant/
+	curl -O -L https://github.com/IDPF/epubcheck/releases/download/v3.0/epubcheck-3.0.zip && unzip epubcheck-3.0.zip
+	chown -R vagrant:vagrant ./*
+	mv epubcheck-3.0/ bin/
 fi
-if [ ! -d "~/kindlegen" ]; then
-cd /home/vagrant/
-chown -R vagrant:vagrant ./*
-curl -o ~/kindlegen http://softcover-binaries.s3.amazonaws.com/kindlegen && chmod +x ~/kindlegen
+if [ -d "/home/vagrant/bin/kindlegen" ]; then
+	echo "skip kindlegen installation"
+else
+	echo "install kindlegen for softcover"
+	su -l vagrant -c "curl -o /home/vagrant/bin/kindlegen http://softcover-binaries.s3.amazonaws.com/kindlegen && chmod +x /home/vagrant/bin/kindlegen"
+fi
+
+if which softcover >/dev/null; then
+    echo "check if the softcover dependencies are satisfied"
 fi
