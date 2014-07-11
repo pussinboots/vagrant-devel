53b11d20a6ae792ab50e88ab479c1797c5814f25
Frank <pussinboots666@googlemail.com>
2014-07-11 14:02:30 +0200

    Set download of play 2.2.3 in no verbose mode

--- provision/provision.sh
+++ provision/provision.sh
@@ -158,7 +158,7 @@ else
 	echo "install play 2.2.3"
 	mkdir -p /home/vagrant/workspace/devel/play
 	cd /home/vagrant/workspace/devel/play
-	wget –quiet http://downloads.typesafe.com/play/2.2.3/play-2.2.3.zip
+	wget –nv http://downloads.typesafe.com/play/2.2.3/play-2.2.3.zip
 	cd /home/vagrant/workspace/devel/play
 	unzip play-2.2.3.zip
 	echo "export PATH=$PATH:/home/vagrant/workspace/devel/play/play-2.2.3" > /etc/profile.d/play.sh
